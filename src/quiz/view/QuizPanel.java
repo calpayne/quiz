2006f1a079a0dbc6ed8cbb86cef3aba34e4f3eee
calpayne <34384159+calpayne@users.noreply.github.com>
2018-10-21 21:51:50 +0100

    Started question count

--- src/quiz/view/QuizPanel.java
+++ src/quiz/view/QuizPanel.java
@@ -7,6 +7,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import quiz.model.MultipleChoiceQuestion;
 
 /**
@@ -16,13 +17,20 @@ import quiz.model.MultipleChoiceQuestion;
 public class QuizPanel extends JPanel {
     
     private final JPanel questionsContainer;
+    private int count;
+    private int maxCount;
     
     public QuizPanel() {
         CardLayout layout = new CardLayout();
+        count = 0;
+        maxCount = 2;
         layout.setHgap(10);
         layout.setVgap(10);
         questionsContainer = new JPanel();
         questionsContainer.setLayout(layout);
+        
+        JProgressBar progress = new JProgressBar(0, maxCount);
+        progress.setValue(count);
         
         String[] choices = {"2001", "2002", "1999", "1995"};
         questionsContainer.add(new QuestionPanel(new MultipleChoiceQuestion("When was Java made?", choices, 4)));
@@ -31,18 +39,22 @@ public class QuizPanel extends JPanel {
         JButton previous = new JButton("Previous");
         previous.addActionListener((ActionEvent ae) -> {
             layout.previous(questionsContainer);
+            count--;
+            progress.setValue(count);
         });
         
         JButton next = new JButton("Next");
         next.addActionListener((ActionEvent ae) -> {
             layout.next(questionsContainer);
+            count++;
+            progress.setValue(count);
         });
         
         GridBagConstraints c = new GridBagConstraints();
         this.setLayout(new GridBagLayout());
         c.gridx = 0;
         c.gridy = 0;
-        c.gridwidth = 2;
+        c.gridwidth = 3;
         c.insets = new Insets(10, 10, 10, 10);
         this.add(questionsContainer, c);
         c.gridx = 0;
@@ -51,6 +63,10 @@ public class QuizPanel extends JPanel {
         c.anchor = GridBagConstraints.WEST;
         this.add(previous, c);
         c.gridx = 1;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.CENTER;
+        this.add(progress, c);
+        c.gridx = 2;
         c.gridy = 1;
         c.anchor = GridBagConstraints.EAST;
         this.add(next, c);
